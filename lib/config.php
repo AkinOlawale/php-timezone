47c5ce1ea183d3ac3a3200254a3e8aeee4bb4529
Akintunde-Rotimi <tundeolawale71@yahoo.com>
2020-04-07 09:50:11 +0200

    Added comments to config

--- lib/config.php
+++ lib/config.php
@@ -2,6 +2,6 @@
 
 session_start();
 
-define('PRODUCTION', false);
-define('DEFAULT_TIMEZONE', 'Africa/Johannesburg');
-define('DISPLAY_ERRORS', true);
+define('PRODUCTION', false); #are we in the production environment? If we are this class will get the timezone dynamically
+define('DEFAULT_TIMEZONE', 'Africa/Johannesburg'); #Tell the class the default timezone to use in development. In production this class uses UTC as default
+define('DISPLAY_ERRORS', true); #display errors?
